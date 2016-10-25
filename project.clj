253d3568fe653e4825ef681e4006273bb6fab6a1
ESC <youhavethewrong@gmail.com>
2016-10-25 10:54:12 -0500

    Implement phrases and topics API calls.  Save captured call results for further analysis.  Save sources of data.

--- project.clj
+++ project.clj
@@ -4,7 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [clj-http "3.3.0"]]
+                 [clj-http "3.3.0"]
+                 [org.clojure/data.json "0.2.6"]]
   :main ^:skip-aot azure-api-poc.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/azure_api_poc/core.clj
+++ src/azure_api_poc/core.clj
@@ -1,10 +1,88 @@
 (ns azure-api-poc.core
   (:require
-   [clj-http.client :as client])
+   [clj-http.client :as client]
+   [clojure.data.json :as json]
+   [clojure.string :as str])
   (:gen-class))
 
+(def sub-key "cdd27a149c7542e3b543579b08a05f72")
+(def text-analytics-url "https://westus.api.cognitive.microsoft.com/text/analytics/v2.0/")
+(def phrases-url (str text-analytics-url "keyPhrases"))
+(def topics-url (str text-analytics-url "topics"))
 
-(def base-url "https://westus.api.cognitive.microsoft.com/text/analytics/v2.0/topics")
+(def corpii
+  ["abominations.of.yondo.txt"
+   "last.incantation.txt"
+   "last.hieroglyph.txt"])
+
+(defn slurp-corpus
+  [text]
+  (slurp (clojure.java.io/resource text)))
+
+(defn parse-corpus
+  [text]
+  (str/split text #"\n"))
+
+(defn chunk-corpus
+  [text-seq prefix]
+  (->> text-seq
+       (map-indexed
+        (fn [i t]
+          {:id (str prefix "-" i)
+           :text t}))
+       (filter #(not (str/blank? (:text %))))
+       vec))
+
+(defn build-body
+  [texts]
+  (vec (flatten (map #(chunk-corpus (parse-corpus (slurp-corpus %)) %) texts))))
+
+(defn topics-body-template
+  [texts]
+  (json/write-str
+   {:stopWords []
+    :topicsToExclude []
+    :documents (build-body texts)}))
+
+(defn phrases-body-template
+  [texts]
+  (json/write-str
+   {:documents (mapv
+                #(assoc % :language "en")
+                (build-body texts))}))
+
+(defn analyze-text
+  [url body-fn texts]
+  (let [options {:content-type :json
+                 :accept :json
+                 :throw-exceptions false
+                 :headers {"Ocp-Apim-Subscription-Key" sub-key}
+                 :body (body-fn texts)}]
+    (client/post url options)))
+
+(defn analyze-text-topics
+  [texts]
+  (analyze-text (str topics-url "?minimumNumberOfDocuments=10") topics-body-template texts))
+
+(defn analyze-text-phrases
+  [texts]
+  (analyze-text phrases-url phrases-body-template texts))
+
+(defn get-analysis
+  [location]
+  (client/get location
+              {:accept :json
+               :throw-exceptions false
+               :headers {"Ocp-Apim-Subscription-Key" sub-key}}))
+
+(comment
+  (analyze-text-phrases corpii)
+
+  (analyze-text-topics corpii)
+  ;; get from 202
+  (get-analysis "https://westus.api.cognitive.microsoft.com/text/analytics/v2.0/operations/f999c42315034ffcb0874b877cb2019b")
+  (get-analysis "https://westus.api.cognitive.microsoft.com/text/analytics/v2.0/operations/2ed3b844752d4ad8aefa52ab56df27ce")
+  )
 
 (defn -main
   "I don't do a whole lot ... yet."
